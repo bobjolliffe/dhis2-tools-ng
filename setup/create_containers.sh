c137133daf88801c055699c17c4c759b036bd39e
bobjolliffe <bobjolliffe@gmail.com>
2019-09-26 15:25:01 +0000

    1.  removed automatic certbot certificate fetch 2.  removed assumption that host interface is eth0

--- setup/create_containers.sh
+++ setup/create_containers.sh
@@ -24,11 +24,13 @@ for CONTAINER in $CONTAINERS; do
   # create nat rules for proxy
   if [[  $TYPE =~ .*_proxy ]] && sudo grep -v -q "^\*nat" /etc/ufw/before.rules; then 
     tmp=$(mktemp)
+    INTERFACE=$(ifconfig |grep  -o '^[a-z].*:' |head -1 |sed 's/.$//')
     sudo cat configs/ufw_proxy /etc/ufw/before.rules > $tmp
     sed -i "s/PROXY_IP/${IP}/g" $tmp
     # FIX THIS
     #sed -i "s/LXD_NETWORK/${NETWORK}/g" $tmp
     sed -i "s/LXD_NETWORK/192.168.0.0\/24/g" $tmp
+    sed -i "s/INTERFACE/$INTERFACE/" $tmp
 
     sudo mv $tmp /etc/ufw/before.rules
     sudo chown root.root /etc/ufw/before.rules
