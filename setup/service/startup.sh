2db37a9535a3cf6b20e26c71b196d8b099c3e129
bobjolliffe <bobjolliffe@gmail.com>
2020-02-11 13:22:50 +0000

    Removed encrypted disk startup file and tidied tomcat

--- setup/service/startup.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-# Echo starting up
-sudo cryptsetup open /dev/sda3 cryptdata
-sudo mount /dev/mapper/cryptdata /mnt/cryptdata
-swapon /mnt/crypdata/swapfile
-
-lxc start postgres
-lxc start tracker
