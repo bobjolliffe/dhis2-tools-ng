67c2bf57717524ec136196c1eaeeb2d6ae931195
bobjolliffe <bobjolliffe@gmail.com>
2020-02-23 21:23:31 +0000

    Fixed unzip switch and tomcat9 error

--- setup/install_scripts.sh
+++ setup/install_scripts.sh
@@ -2,6 +2,6 @@
 
 echo "Installing service scripts"
 sudo cp service/* /usr/local/bin
-sudo mkdir /usr/local/etc/dhis
+sudo mkdir -p /usr/local/etc/dhis
 sudo cp tomcat_setup /usr/local/etc/dhis
 echo "Done"
