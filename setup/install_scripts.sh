d1d8144542f7e7687727ba528a33f9486fa3fad0
bobjolliffe <bobjolliffe@gmail.com>
2020-03-25 14:37:31 +0300

    Fixed up munin tomcat plugin

--- setup/install_scripts.sh
+++ setup/install_scripts.sh
@@ -3,5 +3,5 @@
 echo "Installing service scripts"
 sudo cp service/* /usr/local/bin
 sudo mkdir -p /usr/local/etc/dhis
-sudo cp tomcat_setup /usr/local/etc/dhis
+sudo cp etc/* /usr/local/etc/dhis
 echo "Done"
