26f6feadf6c2d20aa9ab37f90f237fb03380be4c
Bob Jolliffe <bobjolliffe@gmail.com>
2021-02-22 13:31:39 +0000

    Improved backup script defaults

--- setup/install_scripts.sh
+++ setup/install_scripts.sh
@@ -1,7 +1,32 @@
 #!/usr/bin/env bash
 
 echo "Installing service scripts"
-sudo cp service/* /usr/local/bin
-sudo mkdir -p /usr/local/etc/dhis
-sudo cp etc/* /usr/local/etc/dhis
+cp service/* /usr/local/bin
+
+# set restricted permissions on copied files
+umask 177
+
+mkdir -p /usr/local/etc/dhis
+for FILE in $(find etc/*); do
+  BASE=$(basename $FILE)
+	if [ -f /usr/local/etc/dhis/$BASE ]; then
+     echo "$BASE already exists, not over-writing"
+  else
+     cp $FILE /usr/local/etc/dhis
+  fi
+done
+
+if [ -f /etc/cron.d/dhis ]; then
+  echo "DHIS2 cron already exists"
+else
+  cat << EOF > /etc/cron.d/dhis
+# CRON jobs for DHIS2
+PATH=/snap/bin:/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
+
+# Run backup script at 8:25pm (adjust and uncomment) 
+# 25 20 * * *     root /usr/local/bin/dhis2-backup
+EOF
+fi
+
+ 
 echo "Done"
--- setup/lxd_setup.sh
+++ setup/lxd_setup.sh
@@ -9,5 +9,8 @@ snap install --stable lxd
 # initializing lxd system
 cat configs/lxd_preseed | sudo lxd init --preseed
 
+sudo ufw allow in on lxdbr0
+sudo ufw allow out on lxdbr0
+
 # Create the containers
 source create_containers.sh
