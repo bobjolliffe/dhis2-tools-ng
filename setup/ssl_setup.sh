affe1bbea641a8124e2f4ce4bcb41b654156b6a2
root <root@workshop2dhis2.solidlines.io>
2021-06-08 22:21:38 +0000

    Nginx config setup

--- setup/ssl_setup.sh
+++ setup/ssl_setup.sh
@@ -25,12 +25,39 @@ select yn in "Yes" "No"; do
     esac
 done
 
-lxc exec proxy -- service apache2 stop 
-lxc exec proxy -- certbot certonly --non-interactive --standalone --agree-tos -m $EMAIL -d $FQDN
-lxc exec proxy -- a2dissite 000-default
-lxc exec proxy -- a2ensite apache-dhis2
-lxc exec proxy -- service apache2 reload
-lxc exec proxy -- service apache2 start
-# setup auto renewal
-lxc exec proxy --  sed -i 's/0.*$/0 3 * * * root certbot renew --standalone --pre-hook="service apache2 stop" --post-hook="service apache2 start"/' /etc/cron.d/certbot 
+if [[ $PROXY == nginx ]]; then
+	lxc exec proxy -- service nginx stop
+	lxc exec proxy -- certbot certonly -d $FQDN --standalone -m $EMAIL --agree-tos -n --no-eff-email
 
+	####
+	# ssl-files.conf
+	####
+cat <<EOF > /tmp/ssl-files.conf
+ssl_certificate /etc/letsencrypt/live/${FQDN}/fullchain.pem;
+ssl_certificate_key /etc/letsencrypt/live/${FQDN}/privkey.pem;
+EOF
+
+	lxc file push /tmp/ssl-files.conf proxy/etc/nginx/conf.d/ssl-files.conf
+	#rm /tmp/ssl-files.conf
+	
+	cat configs/nginx-dhis2.conf |sed "s/FQDN/${FQDN}/" > /tmp/nginx.conf
+	sed -i "s/listen 80;/listen 443 ssl http2;/g" /tmp/nginx.conf
+	sed -i "s/#REDIRECT //g" /tmp/nginx.conf
+
+	lxc file push /tmp/nginx.conf proxy/etc/nginx/nginx.conf
+	#rm /tmp/nginx.conf
+	# setup auto renewal
+	lxc exec proxy --  echo '0 3 * * * root certbot renew --standalone --pre-hook="service nginx stop" --post-hook="service nginx start"/' > /etc/cron.d/certbot
+
+	lxc exec proxy -- service nginx start
+elif [[ $PROXY == apache ]]; then
+	lxc exec proxy -- service apache2 stop 
+	lxc exec proxy -- certbot certonly --non-interactive --standalone --agree-tos -m $EMAIL -d $FQDN
+	lxc exec proxy -- a2dissite 000-default
+	lxc exec proxy -- a2ensite apache-dhis2
+	lxc exec proxy -- service apache2 reload
+	lxc exec proxy -- service apache2 start
+	# setup auto renewal
+	lxc exec proxy --  echo '0 3 * * * root certbot renew --standalone --pre-hook="service nginx stop" --post-hook="service nginx start"/' > /etc/cron.d/certbot
+
+fi
